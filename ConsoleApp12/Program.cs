09ae0a3d052b730b2047e9d5e36a5629eacf39f2
chirv <chirv@DESKTOP-LHI0AR3>
2021-12-23 16:52:28 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Csh_console_tasks
+{
+    class thread2141726
+    {
+        static void Main(string[] args)
+        {
+            UInt32 count;
+            Console.Write("Введите число: ");
+            count = Convert.ToUInt32(Console.ReadLine());
+            Console.Write("Число комбинаций: ");
+            Console.WriteLine(Math.Pow(2, count));
+        }
+    }
+}
